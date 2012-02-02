8252a5e49797c682572178e0712f821944db9910
Prutha <desai_234@yahoo.com>
2012-02-02 02:52:36 -0800

    HelloFunc.c - Simple Update

---
+++ hellofunc.c
@@ -0,0 +1,14 @@
+/*
+Name: HelloFunc.c
+Author: Prutha
+Purpose: Contains simple function body that prints "hello world!!" on screen
+*/
+
+#include <stdio.h>
+#include "hellofunc.h"
+
+void myPrintHelloWorld(void){
+
+  printf("Hello World!!\n");
+	return;
+}
