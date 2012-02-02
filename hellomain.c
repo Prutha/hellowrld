050ba91a7b1bf6a0a5fe2b3a963177984d060cb0
Prutha <desai_234@yahoo.com>
2012-02-02 02:54:36 -0800

    hellomain.c - Simple Update

---
+++ hellomain.c
@@ -0,0 +1,16 @@
+/*
+Name: HelloMain.c
+Author: Prutha
+Purpose: Contains main function
+*/
+
+#include <stdio.h>
+#include "hellofunc.h"
+
+int main(){
+
+  // Call function in another file
+	myPrintHelloWorld();
+
+	return 0;
+}
